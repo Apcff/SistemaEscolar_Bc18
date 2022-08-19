ed52b691bc214226576ed6cea741a47c286a4faf
Ana Paula <anapaulacff@outlook.com>
2022-08-19 10:17:12 -0300

    Criação componente nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
-import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { NavComponent } from './components/nav/nav.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
