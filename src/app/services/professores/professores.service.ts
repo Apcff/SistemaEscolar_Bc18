e9481627e074d83cc804539109ba9a820b020cb0
Ana Paula <anapaulacff@outlook.com>
2022-09-06 12:24:06 -0300

    serviços professores

---
+++ src/app/services/professores/professores.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from 'src/app/config/api.config';
+import { Professor } from 'src/app/core/models/professor';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfessoresService {
+  professoresUrl = `${API_CONFIG.baseUrl.prod}/service/professores`;
+  constructor(private http: HttpClient) {}
+
+  findAll(): Observable<Professor[]> {
+    return this.http.get<Professor[]>(this.professoresUrl);
+  }
+
+  findById(id: number): Observable<Professor> {
+    return this.http.get<Professor>(`${this.professoresUrl}/${id}`);
+  }
+
+  create(professor: Professor) {
+    return this.http.post(this.professoresUrl, professor);
+  }
+
+  update(professor: Professor) {
+    return this.http.put(`${this.professoresUrl}/${professor.id}`, professor);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.professoresUrl}/${id}`);
+  }
+}
