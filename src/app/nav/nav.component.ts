f3fff5dd76b7fdebf7047f5da04877ac643ac141
Ana Paula <anapaulacff@outlook.com>
2022-09-06 11:59:16 -0300

    Implementação componente nav, Html e Css

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    ) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['nav'])
+   
+  }
+
+}
